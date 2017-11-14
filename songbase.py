13a468e497c85d8a3316cf59a690ec2dbfeff467
Raveena Wadhwa <rwadhwa@udel.edu>
2017-11-14 13:42:19 -0500

    GET and POST

--- songbase.py
+++ songbase.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'raveenaisgreat'
 
 @app.route('/')
 def home():
     #return '<h1>hello world!!! lala<h1>'
     return render_template('index.html')
+
+@app.route('/form-demo', methods = ['GET','POST'])
+def form_demo():
+    if request.method == 'GET':
+        first_name= request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            first_name= session.get('first_name')
+            return render_template('form-demo.html', first_name=first_name)
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        #return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))
 
 
 @app.route('/songs')
