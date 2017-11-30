b32ecfd2513e6258d5a6b7e29ed698ea2148e74f
Raveena Wadhwa <rwadhwa@udel.edu>
2017-11-30 13:13:33 -0500

    part way

--- songbase.py
+++ songbase.py
@@ -16,7 +16,7 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     about = db.Column(db.Text)
-    songs = db.relationship('Song', backref='artist')
+    songs = db.relationship('Song', backref='artist', cascade='delete')
 
 
 class Song(db.Model):
@@ -71,6 +71,16 @@ def edit_artists(id):
         # get data from the form
         artist.name = request.form['name']
         artist.about = request.form['about']
+        db.session.commit()
+        return redirect(url_for('show_all_artists'))
+
+@app.route('/artist/delete/<int:id>', methods=['GET', 'POST'])
+def delete_artists(id):
+    artist = Artist.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('artist-delete.html', artist=artist)
+    if request.method == 'POST':
+        db.session.delete(artist)
         db.session.commit()
         return redirect(url_for('show_all_artists'))
 
